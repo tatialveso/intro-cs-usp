34b4190e3c4c4fde478f5d46cd5eddb731fa0553
tatialveso <tatiialveso@gmail.com>
2022-01-24 13:45:45 -0300

    :sparkles: complete 2nd exercice

---
+++ lista-um/medias.py
@@ -0,0 +1,8 @@
+firstGrade = int(input("Digite a primeira nota: "))
+secondGrade = int(input("Digite a segunda nota: "))
+thirdGrade = int(input("Digite a terceira nota: "))
+fourthGrade = int(input("Digite a quarta nota: "))
+
+avg = (firstGrade + secondGrade + thirdGrade + fourthGrade) / 4
+
+print("A média aritmética é", avg)
